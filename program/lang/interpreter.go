332b4895407b1d459fba9b6b789012d55e651f56
vili1120 <vili1120@localhost.my.domain>
2025-04-26 15:34:50 +0200

    Vasm v0.2.5

--- program/lang/interpreter.go
+++ program/lang/interpreter.go
@@ -230,9 +230,46 @@ defer rl.CloseWindow()
     }
   case instructions["RL:EXEC"]:
     *i.RLcode += "\n}"
+    mod := `
+module vasm
+
+go 1.24.2
+
+require github.com/gen2brain/raylib-go/raylib v0.0.0-20250409052854-a4292f0f0412
+
+require (
+	github.com/ebitengine/purego v0.7.1 // indirect
+	golang.org/x/exp v0.0.0-20240506185415-9bf2ced13842 // indirect
+	golang.org/x/sys v0.20.0 // indirect
+)
+    `
+    sum := `
+github.com/ebitengine/purego v0.7.1 h1:6/55d26lG3o9VCZX8lping+bZcmShseiqlh2bnUDiPA=
+github.com/ebitengine/purego v0.7.1/go.mod h1:ah1In8AOtksoNK6yk5z1HTJeUkC1Ez4Wk2idgGslMwQ=
+github.com/gen2brain/raylib-go/raylib v0.0.0-20250409052854-a4292f0f0412 h1:1ilXP20QHDAM0Vl6D9SNoNs6x+iyeV1TYsTZaltOLQY=
+github.com/gen2brain/raylib-go/raylib v0.0.0-20250409052854-a4292f0f0412/go.mod h1:BaY76bZk7nw1/kVOSQObPY1v1iwVE1KHAGMfvI6oK1Q=
+golang.org/x/exp v0.0.0-20240506185415-9bf2ced13842 h1:vr/HnozRka3pE4EsMEg1lgkXJkTFJCVUX+S/ZT6wYzM=
+golang.org/x/exp v0.0.0-20240506185415-9bf2ced13842/go.mod h1:XtvwrStGgqGPLc4cjQfWqZHG1YFdYs6swckp8vpsjnc=
+golang.org/x/sys v0.20.0 h1:Od9JTbYCk261bKm4M/mw7AklTlFYIa0bIp9BgSm1S8Y=
+golang.org/x/sys v0.20.0/go.mod h1:/VUhepiaJMQUp4+oa/7Zr1D23ma6VTLIYjOOTFZPUcA=
+    `
+    os.Mkdir("vasmoutdir", 0755)
+    os.Chdir("vasmoutdir")
     fmt.Println("Generating Raylib Go code...")
 
     err := os.WriteFile("out.go", []byte(*i.RLcode), 0644)
+    if err != nil {
+      fmt.Println("Error writing Go file:", err)
+      os.Exit(1)
+    }
+
+    err = os.WriteFile("go.mod", []byte(mod), 0644)
+    if err != nil {
+      fmt.Println("Error writing Go file:", err)
+      os.Exit(1)
+    }
+
+    err = os.WriteFile("go.sum", []byte(sum), 0644)
     if err != nil {
       fmt.Println("Error writing Go file:", err)
       os.Exit(1)
@@ -249,6 +286,10 @@ defer rl.CloseWindow()
       os.Exit(1)
     }
     os.Remove("out.go")
+    os.Remove("go.mod")
+    os.Remove("go.sum")
+    os.Chdir("../")
+    os.Remove("vasmoutdir")
 
     // ✅ Keep advancing
     i.Advance()
